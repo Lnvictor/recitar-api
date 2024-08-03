5a634947444fd6f7c624e1fa33d9df5fc837c942
Lnvictor <victorhpereira26@gmail.com>
2024-08-03 11:08:33 -0300

    Spring Bootstrap project

---
+++ src/test/java/ccb/smonica/recitar_api/RecitarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ccb.smonica.recitar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecitarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
